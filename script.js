cbca6219c6f90842f1b31a3a5b1a6748e8e5c878
lakshitha <lsmakandura@gmail.com>
2021-03-31 13:01:37 +0530

    updated

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ function computeLoan(){
 
     var amount=document.getElementById("amount").value;
     var interest_rate=document.getElementById("interest_rate").value;
+    if (interest_rate>50){
+        alert("Rate is exceeded 50");
+    }
     var months =document.getElementById("months").value;
     var interest=(amount* (interest_rate * .01)) / months;
     var payment=((amount/months)+interest).toFixed(2);
     payment=payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     document.getElementById("payment").innerHTML="Monthly Payment = Rs:"+payment;
+}
+function hide(){
+    document.getElementById("payment").innerHTML=" ";
 }
